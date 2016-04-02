299981f820d8dc8f7ac4305ada6d8c08c1ff333b
Jason <jasonfontaine18@gmail.com>
2016-04-02 00:20:50 -0400

    things i forgot

--- src/com/rdc/Test.java
+++ src/com/rdc/Test.java
@@ -23,35 +23,35 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 public class Test {
 
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
+    @RequestMapping(value="/test", method= RequestMethod.GET)
     public ModelAndView test(HttpServletRequest req, ModelMap map){
 
 
-        UserService userSer = UserServiceFactory.getUserService();
-        User currentUser = userSer.getCurrentUser();
-
-       // if (currentUser != null) {
-         //   String userName = currentUser.getEmail();
-            Result<UserData> user = ofy().load().key(Key.create(UserData.class, "hello")); //query to make check if the User is in the db or not
-
-            if (user.now() == null) {// if not in the db then create it
-                UserData newUserData = new UserData();//create a new User to with its email and score set to 0 to be save to db later
-                newUserData.setEmail("hello");
-                newUserData.setScore(0);
-                ofy().save().entity(newUserData).now();//save the new User in db
-            }
-
-            /*increase the User's score by one
-            Result<UserData> player = ofy().load().key(Key.create(UserData.class, "hello"));
-            UserData userDataInDB = player.now();
-            userDataInDB.setScore(userDataInDB.getScore() + 1);
-            ofy().save().entity(userDataInDB).now();
-
-            System.out.println(userDataInDB.getEmail() + " score: " + userDataInDB.getScore());
-            */
-        //}
-
-        return new ModelAndView("test");
+//        UserService userSer = UserServiceFactory.getUserService();
+//        User currentUser = userSer.getCurrentUser();
+//
+//       // if (currentUser != null) {
+//         //   String userName = currentUser.getEmail();
+//            Result<UserData> user = ofy().load().key(Key.create(UserData.class, "hello")); //query to make check if the User is in the db or not
+//
+//            if (user.now() == null) {// if not in the db then create it
+//                UserData newUserData = new UserData();//create a new User to with its email and score set to 0 to be save to db later
+//                newUserData.setEmail("hello");
+//                newUserData.setScore(0);
+//                ofy().save().entity(newUserData).now();//save the new User in db
+//            }
+//
+//            /*increase the User's score by one
+//            Result<UserData> player = ofy().load().key(Key.create(UserData.class, "hello"));
+//            UserData userDataInDB = player.now();
+//            userDataInDB.setScore(userDataInDB.getScore() + 1);
+//            ofy().save().entity(userDataInDB).now();
+//
+//            System.out.println(userDataInDB.getEmail() + " score: " + userDataInDB.getScore());
+//            */
+//        //}
+//
+        return new ModelAndView("homepage");
 
     }
 
