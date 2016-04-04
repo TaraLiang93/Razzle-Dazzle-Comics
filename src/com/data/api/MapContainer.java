843aa891f56b420dff5e54cb5d1e31e9bb238012
jamhuang <james.huang@stonybrook.edu>
2016-04-04 19:28:50 -0400

    Working on GetScribblesCommand. Thinking of adding a MapContainer which takes a map. so that the form of reading from the database stays the same.

--- src/com/data/api/MapContainer.java
+++ src/com/data/api/MapContainer.java
@@ -1,7 +1,36 @@
 package com.data.api;
 
+import com.googlecode.objectify.Key;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Zhenya on 4/4/16.
  */
-public class MapContainer {
+public class MapContainer <T> implements Container<T>{
+
+    List<T> listOfEntities;
+    public MapContainer (Map<Key<T>, T> map){
+
+        for (Map.Entry<Key<T>, T> entity : map.entrySet()){
+            listOfEntities.add( entity.getValue() ); //get value is of the type T
+        }
+    }
+
+    @Override
+    public List<T> getList() {
+        return listOfEntities;
+    }
+
+    @Override
+    public T getResult() {
+        if( listOfEntities != null){
+            return listOfEntities.get(0);
+        }
+        else{
+            //perhaps throw exception?
+            return null;
+        }
+    }
 }
