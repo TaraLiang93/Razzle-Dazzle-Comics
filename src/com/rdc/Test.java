2cfbe38215583eaafc6017a4312766cc5eba49e8
jamhuang <james.huang@stonybrook.edu>
2016-04-05 23:03:02 -0400

    Creatables and fillCommands for Doodles, Scribbles, Usedata should be done. Need to test them. After that check the creatable and fillcommand for tag.

--- src/com/rdc/Test.java
+++ src/com/rdc/Test.java
@@ -4,20 +4,23 @@ import com.data.UserData;
 
 import com.data.api.DoodleQueries.GetDoodlesByIDCommand;
 import com.data.api.DoodleQueries.GetDoodlesByTagCommand;
-import com.data.api.DoodleQueries.GetScribblesCommand;
 import com.data.api.Readable;
+import com.data.api.createables.ScribbleCreater;
+import com.data.api.createables.TagCreater;
+import com.data.api.createables.UserDataCreater;
+import com.data.api.dataItems.NoWork;
+import com.data.api.dataItems.ScribbleCommandFill;
+import com.data.api.dataItems.TagCommandFill;
+import com.data.api.dataItems.UserDataCommandFill;
 import com.data.creation.Doodle;
+import com.data.creation.Page;
 import com.data.creation.Scribble;
 import com.data.structure.Tag;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.repackaged.com.google.protobuf.Syntax;
 import com.googlecode.objectify.Key;
-import com.googlecode.objectify.LoadResult;
-import com.googlecode.objectify.Result;
 import com.googlecode.objectify.cmd.Query;
-import com.javafx.tools.doclets.formats.html.SourceToHTMLConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +29,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
@@ -116,13 +120,110 @@ public class Test {
 
 
 
-//        Readable<Scribble> getUserScribbles = new GetScribblesCommand(james.getKey());
-//        getUserScribbles.fetch().getList();
-
 
 
         return new ModelAndView("test");
 
     }
+
+    @RequestMapping(value="/test2", method= RequestMethod.GET)
+    public ModelAndView test2(HttpServletRequest req, ModelMap map){
+
+        UserData userData = new UserData();
+        userData.setNickName("Charles Junior");
+        ofy().save().entity(userData).now();
+
+        Scribble scribble1 = new Scribble();
+        scribble1.setTitle("I am Scribble One");
+        ofy().save().entity(scribble1).now();
+
+        Scribble scribble2 = new Scribble();
+        scribble2.setTitle("I am Scribble Two");
+        ofy().save().entity(scribble2).now();
+
+        Scribble scribble3 = new Scribble();
+        scribble3.setTitle("I am Scribble Three");
+        ofy().save().entity(scribble3).now();
+
+        userData.addScribbleToList(scribble1.getKey());
+        userData.addScribbleToList(scribble2.getKey());
+        userData.addScribbleToList(scribble3.getKey());
+
+        ofy().save().entity(userData).now();
+
+
+
+
+        // this tells the test2.JSP that "user" refers to userData a UserData instance
+
+        Tag tag1 = new Tag();
+        tag1.setName("I am tag 1");
+        tag1.setUserData(userData.getKey());
+        ofy().save().entity(tag1).now();
+
+        Tag tag2 = new Tag();
+        tag2.setName("I am tag 2");
+        tag2.setUserData(userData.getKey()); // set parent using setUserData
+        ofy().save().entity(tag2).now();
+
+        Tag tag3 = new Tag();
+        tag3.setName("I am tag 3");
+        tag3.setUserData(userData.getKey());
+        ofy().save().entity(tag3).now();
+
+        userData.addTagToList( tag1.getKey());
+        userData.addTagToList( tag2.getKey());
+        userData.addTagToList( tag3.getKey());
+
+        ofy().save().entity(userData).now();
+
+
+        Doodle doodle1 = new Doodle();
+        doodle1.setTitle("I am Doodle 1");
+        ofy().save().entity(doodle1).now();
+
+        Doodle doodle2 = new Doodle();
+        doodle2.setTitle("I am Doodle 2");
+        ofy().save().entity(doodle2).now();
+
+        Doodle doodle3 = new Doodle();
+        doodle3.setTitle("I am Doodle 3");
+        ofy().save().entity(doodle3).now();
+
+        userData.addDoodleToList( doodle1.getKey() );
+        userData.addDoodleToList( doodle2.getKey() );
+        userData.addDoodleToList( doodle3.getKey() );
+        ofy().save().entity(userData);
+
+        map.put("user", userData);
+
+        System.out.println( " The name of the owner of " + tag1.getName() + " is " + tag1.getUserData().getNickName() );
+
+        TagCreater tagCreater = new TagCreater("I am tagicus", userData.getKey());
+//        Tag t = tagCreater.createEntity(new NoWork<Tag>());
+
+        Tag t = tagCreater.createEntity(new TagCommandFill());
+
+        ScribbleCreater scribbleCreater = new ScribbleCreater();
+        List<Key<Page>> pageList = new ArrayList<>();
+        Scribble scribble =  scribbleCreater.createEntity( new ScribbleCommandFill(pageList) );
+
+
+        return new ModelAndView("test2");
+    }
+
+
+//    @RequestMapping(value="/test3", method= RequestMethod.GET)
+//    public ModelAndView test3(HttpServletRequest req, ModelMap map){
+//        /**
+//         * testing using the creater objects
+//         */
+//
+//        UserDataCreater userDataCreater = new UserDataCreater(); // fill userDataCreater with initial data
+//        System.out.println( userDataCreater.createEntity(new UserDataCommandFill()) );
+//
+////        map.put();
+//        return new ModelAndView("test3");
+//    }
 
 }
