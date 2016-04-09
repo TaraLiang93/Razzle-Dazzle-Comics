94e7cc69b9fa4fd52a5bf362b2c4f121c6594f51
jamhuang <james.huang@stonybrook.edu>
2016-04-09 00:32:54 -0400

    Can now create, read, and update Doodles, Scribbles, and Userdata. TODO: refactor commands like DoodleCommandFill to DoodleFillCommand. Use spring model to fill in Scribbles.

---
+++ src/com/data/api/createables/ChapterCreater.java
@@ -0,0 +1,14 @@
+package com.data.api.createables;
+
+import com.data.api.Createable;
+import com.data.creation.Chapter;
+
+/**
+ * Created by Zhenya on 4/9/16.
+ */
+public class ChapterCreater extends Createable<Chapter> {
+    @Override
+    protected Chapter getEntity() {
+        return null;
+    }
+}
