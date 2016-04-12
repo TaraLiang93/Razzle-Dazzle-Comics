dd66ab9cc45369cb37c21f6f05d7e5873b4de4f8
jamhuang <james.huang@stonybrook.edu>
2016-04-12 12:10:27 -0400

    Fixed  UserCreater to take in a User

--- src/com/data/api/createables/UserDataCreater.java
+++ src/com/data/api/createables/UserDataCreater.java
@@ -9,45 +9,27 @@ import com.google.appengine.api.users.User;
  * Created by Zhenya on 4/5/16.
  */
 public class UserDataCreater extends Createable<UserData>{
-    String username;
-    String nickName;
-    String UserDataId;
-
     User user;
 
     public UserDataCreater(User user){ //TODO : Do error Validation
         this.user = user;
     }
 
-    public UserDataCreater(String username, String nickName, String UserDataId){
-        this.username = username;
-        this.nickName = nickName;
-        this.UserDataId = UserDataId;
-        //TODO : load default profile image
-    }
 
 
     @Override
     protected UserData getEntity() throws CreateException { //TODO : Throw exceptions
 
         UserData userData = new UserData();
-
+        //TODO : load default profile image
         if( user == null ){
             throw new CreateException("UserDataCreater user is null");
         }
 
-        // if username was provided as a parameter then set it
-        if( this.username != null && !(this.username.equals("")) ) {
-            userData.setUserName(this.username);
-        }
-
-        if( this.nickName != null && !(this.nickName.equals("")) ) {
-            userData.setNickName(this.nickName);
-        }
-
-        if( this.UserDataId != null && !(this.UserDataId.equals("")) ) {
-            userData.setUserid(this.UserDataId);
-        }
+        userData.setUserid(user.getUserId());
+        userData.setNickName(user.getNickname());
+        userData.setUserName( user.getEmail());
+        
 
         return userData;
     }
--- src/com/data/api/updatables/updateTasks/UpdateChapterFlowTask.java
+++ src/com/data/api/updatables/updateTasks/UpdateChapterFlowTask.java
@@ -1,6 +1,5 @@
 package com.data.api.updatables.updateTasks;
 
-import com.data.api.exceptions.FetchException;
 import com.data.api.exceptions.UpdateException;
 import com.data.api.interfaces.Container;
 import com.data.api.interfaces.UpdateTask;
@@ -12,14 +11,26 @@ import java.util.List;
  * Created by Zhenya on 4/11/16.
  */
 public class UpdateChapterFlowTask implements UpdateTask<Chapter> {
+    Long flowIdLong;
 
-    public UpdateChapterFlowTask(){
 
+    public UpdateChapterFlowTask(Long flowIdLong){
+        this.flowIdLong = flowIdLong;
+    }
+
+    public UpdateChapterFlowTask(String strId){
+        this.flowIdLong = Long.parseLong(strId);
     }
 
 
     @Override
-    public List<Chapter> update(Container<Chapter> entity) throws UpdateException, FetchException {
+    public List<Chapter> update(Container<Chapter> entity) throws UpdateException {
+        if(this.flowIdLong == null){
+            throw new UpdateException("UpdateChapterFlowTask flowIdLong is null");
+        }
+        else{
+            entity.
+        }
         return null;
     }
 }
