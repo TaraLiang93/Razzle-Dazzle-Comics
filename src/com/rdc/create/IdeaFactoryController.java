7038375390260b1428b279adfefe36360d215a20
jamhuang <james.huang@stonybrook.edu>
2016-04-13 19:00:53 -0400

    fixed fetch for GetUserDataByUserCommand

--- src/com/rdc/create/IdeaFactoryController.java
+++ src/com/rdc/create/IdeaFactoryController.java
@@ -15,7 +15,6 @@ import com.data.api.updatables.updateTasks.UpdateDoodleTask;
 import com.data.creation.Doodle;
 import com.data.creation.Scribble;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.labs.repackaged.org.json.JSONString;
 import com.model.ScribbleModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -103,13 +102,14 @@ public class IdeaFactoryController {
 
         String canvasImage;
 
+        Long newId = new Long("5066549580791808");
+
         try {
-            Readable<Doodle> getDoodle = new GetDoodlesByIDCommand(id);
+            Readable<Doodle> getDoodle = new GetDoodlesByIDCommand(newId);
             Doodle doodle = getDoodle.fetch().getResult();
             map.put("canvasImage",doodle.getCanvas().getCanvasImage());
             map.put("doodleTitle",doodle.getTitle());
             map.put("doodleDescription",doodle.getDescription());
-            map.put("doodleId",doodle.getDoodleId());
         } catch (FetchException e) {
             e.printStackTrace();
         }
@@ -142,7 +142,7 @@ public class IdeaFactoryController {
             try {
                 Doodle theDoodle = getDoodle.fetch().getResult();
                 updateDoodle.updateEntity(getDoodle, new UpdateDoodleTask(doodleTitle, doodleDescription, canvasImage) );
-            } catch (CreateException | FetchException | UpdateException  e) {
+            } catch (FetchException | UpdateException | CreateException  e) {
                 e.printStackTrace();
             }
 
