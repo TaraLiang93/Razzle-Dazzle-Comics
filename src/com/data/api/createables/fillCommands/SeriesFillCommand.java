2fdff2edbd7c5b56e9c37df7c9aaab1a7c4ff45e
jamhuang <james.huang@stonybrook.edu>
2016-04-15 16:13:38 -0400

    SeriesFillCommand Fixed Jason

--- src/com/data/api/createables/fillCommands/SeriesFillCommand.java
+++ src/com/data/api/createables/fillCommands/SeriesFillCommand.java
@@ -2,19 +2,22 @@ package com.data.api.createables.fillCommands;
 
 import com.data.api.exceptions.CreateException;
 import com.data.api.interfaces.FillDataCommand;
+import com.data.structure.Series;
 
 /**
  * Created by Zhenya on 4/15/16.
  */
-public class SeriesFillCommand implements FillDataCommand<SeriesFillCommand> {
+public class SeriesFillCommand implements FillDataCommand<Series> {
 
     public SeriesFillCommand(){
 
     }
 
     @Override
-    public void fillEntity(SeriesFillCommand entity) throws CreateException {
+    public void fillEntity(Series entity) throws CreateException {
         //TODO: Nothing right now cause creater takes model and we will alreadys?
     }
+
+
 
 }
