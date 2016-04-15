475a8d80255adb52f3827b21eda863c6d3dd0a21
jamhuang <james.huang@stonybrook.edu>
2016-04-15 14:23:11 -0400

    Series should be ready for connecting. Create: - Series Creater -SeriesFIllCommand
    Read:
    - GetSeriesByIDCommand
    - GetSeriesofUserDataCommand
    
    Update:
    - SeriesUpdater
    - UpdateSeriesAddChapterTask

--- src/com/data/api/createables/SeriesCreater.java
+++ src/com/data/api/createables/SeriesCreater.java
@@ -4,21 +4,29 @@ import com.data.api.exceptions.CreateException;
 import com.data.api.exceptions.FetchException;
 import com.data.api.interfaces.Createable;
 import com.data.structure.Series;
-import com.google.appengine.api.users.User;
+import com.model.SeriesModel;
 
 /**
  * Created by Zhenya on 4/14/16.
  */
 public class SeriesCreater extends Createable<Series> {
-    User user;
+    SeriesModel seriesModel;
+
+    public SeriesCreater(SeriesModel seriesModel){
+        this.seriesModel = seriesModel;
+    }
 
     @Override
     protected Series getEntity() throws CreateException, FetchException {
+        if( seriesModel == null){
+            throw new CreateException();
+        }
         Series series = new Series();
+        series.setSeriesCover(seriesModel.getSeriesCover());
+        series.setTitle( seriesModel.getTitle());
+        series.setDescription(seriesModel.getDescription());
+        series.setPublished(seriesModel.isPublished());
 
-
-
-
-        return null;
+        return series;
     }
 }
---
+++ src/com/data/api/updatables/updateTasks/SeriesUpdater.java
@@ -0,0 +1,11 @@
+package com.data.api.updatables.updateTasks;
+
+import com.data.api.interfaces.Updateable;
+import com.data.structure.Series;
+
+/**
+ * Created by Zhenya on 4/15/16.
+ */
+//exists to specify the generic
+public class SeriesUpdater extends Updateable<Series> {
+}
