e50629404c45d2cdc0b19f15096b9e3e2a7daa54
jamhuang <james.huang@stonybrook.edu>
2016-04-15 16:46:02 -0400

    UpdateUserDataAddSeriesTask fixed

--- src/com/data/api/updatables/updateTasks/UpdateUserDataAddSeriesTask.java
+++ src/com/data/api/updatables/updateTasks/UpdateUserDataAddSeriesTask.java
@@ -5,8 +5,12 @@ import com.data.api.exceptions.CreateException;
 import com.data.api.exceptions.FetchException;
 import com.data.api.exceptions.UpdateException;
 import com.data.api.interfaces.Container;
+import com.data.api.interfaces.Readable;
 import com.data.api.interfaces.UpdateTask;
+import com.data.api.queries.external.GetSeriesByIDCommand;
+import com.data.structure.Series;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,7 +36,23 @@ public class UpdateUserDataAddSeriesTask implements UpdateTask<UserData> {
 
     @Override
     public List<UserData> update(Container<UserData> entity) throws UpdateException, FetchException, CreateException {
-        return null;
+        if(  this.seriesId == null || entity == null){
+            throw new UpdateException("UpdateUserDataAddDoodlesTask doodleId or entity is null");
+        }
+
+        UserData userData = entity.getResult();
+
+        //get series entity
+        Readable<Series> getSeriesReadable = new GetSeriesByIDCommand(seriesId);
+        Series seriesToAdd = getSeriesReadable.fetch().getResult();
+
+        //updateUserData
+        userData.addSeriesToList(seriesToAdd.getKey());
+
+        List<UserData> updateList = new ArrayList<>();
+        updateList.add(userData);
+
+        return updateList;
     }
 
 }
