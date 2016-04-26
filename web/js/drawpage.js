6e1005d7ddf9c879ba35c14ab4005a98b7b092b5
Jason <jasonfontaine18@gmail.com>
2016-04-25 21:04:16 -0400

    finishing designing drawPage

--- web/js/drawpage.js
+++ web/js/drawpage.js
@@ -12,18 +12,26 @@ $(document).ready(function() {
     });
 
     $(".scene").click(function() {
-        $(".scene.selected").removeClass("selected");
-        $(this).addClass("selected");
 
-        var canvasImg = $(this).find(".canvasImage").text();
-        var diaglogs = $(this).find(".SceneDialogs").html();
-        var setting = $(this).find(".sceneSetting").text();
+        if(!$(this).hasClass("selected")) {
+            // this grabs the data from the selected item and loads it
+            var canvasImg = $(this).find(".canvasImage").text();
+            var diaglogs = $(this).find(".SceneDialogs").html();
+            var setting = $(this).find(".sceneSetting").text();
 
 
-        canvas.loadFromJSON(canvasImg, canvas.renderAll.bind(canvas));
-        canvas.renderAll();
-        $("#SettingScene").text(setting);
-        $(".diaglogs").html(diaglogs);
+            // save the data into the new one
+            $(".scene.selected").find(".canvasImage").text(JSON.stringify(canvas));
+            $(".scene.selected").find(".SceneDialogs").html($(".diaglogs").html());
+            $(".scene.selected").find(".sceneSetting").text($("#SettingScene").text());
+            $(".scene.selected").removeClass("selected");
+            $(this).addClass("selected");
+
+            canvas.loadFromJSON(canvasImg, canvas.renderAll.bind(canvas));
+            canvas.renderAll();
+            $("#SettingScene").text(setting);
+            $(".diaglogs").html(diaglogs);
+        }
 
     });
 
