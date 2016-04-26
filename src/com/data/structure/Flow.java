3917485445040247a711faa5771079c0390a5eaa
jamhuang <james.huang@stonybrook.edu>
2016-04-25 22:23:23 -0400

    added flow, flowTask, flowType TODO: - eventually load default flows

--- src/com/data/structure/Flow.java
+++ src/com/data/structure/Flow.java
@@ -4,6 +4,9 @@ import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by drodrigues on 3/30/16.
  */
@@ -12,9 +15,44 @@ public class Flow {
     @Id
     Long flowId;
 
+    String flowName;
+
+    List<Key<FlowTask>> flowTasks;
+
+    public Flow(){
+        flowTasks  = new ArrayList<>();
+    }
+
     // get the Key of a Doodle
     public Key<Flow> getKey() {
         return Key.create(Flow.class, flowId);
     }
 
+    public Long getFlowId() {
+        return flowId;
+    }
+
+    public void setFlowId(Long flowId) {
+        this.flowId = flowId;
+    }
+
+    public String getFlowName() {
+        return flowName;
+    }
+
+    public void setFlowName(String flowName) {
+        this.flowName = flowName;
+    }
+
+    public List<Key<FlowTask>> getFlowTasks() {
+        return flowTasks;
+    }
+
+    public void setFlowTasks(List<Key<FlowTask>> flowTasks) {
+        this.flowTasks = flowTasks;
+    }
+
+    public void addFlowTaskToFlowTaskList( Key<FlowTask> flowTaskKey){
+        this.flowTasks.add(flowTaskKey);
+    }
 }
--- src/com/data/structure/FlowTask.java
+++ src/com/data/structure/FlowTask.java
@@ -1,10 +1,50 @@
 package com.data.structure;
 
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
 
 /**
  * Created by drodrigues on 3/30/16.
  */
 @Entity
 public class FlowTask {
+    @Id
+    Long flowTaskId;
+
+    String flowTaskName;
+    int index;
+    Key<FlowType> flowTypeKey;
+
+    public Long getFlowTaskId() {
+        return flowTaskId;
+    }
+
+    public void setFlowTaskId(Long flowTaskId) {
+        this.flowTaskId = flowTaskId;
+    }
+
+    public String getFlowTaskName() {
+        return flowTaskName;
+    }
+
+    public void setFlowTaskName(String flowTaskName) {
+        this.flowTaskName = flowTaskName;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Key<FlowType> getFlowTypeKey() {
+        return flowTypeKey;
+    }
+
+    public void setFlowTypeKey(Key<FlowType> flowTypeKey) {
+        this.flowTypeKey = flowTypeKey;
+    }
 }
