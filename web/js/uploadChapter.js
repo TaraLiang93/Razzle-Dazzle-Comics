5712bd62443fc9bb5740fc8227bfc0e1998fc2fc
TaraLiang93 <taraliang93@gmail.com>
2016-04-28 14:44:08 -0400

    jsp and js for upload chapter is done

--- web/js/uploadChapter.js
+++ web/js/uploadChapter.js
@@ -30,45 +30,58 @@ $(document).ready(function(){
            reader.readAsDataURL(event.target.files[0]);
        });
 
+        function editPage(event, page){
+
+        }
+
         function newPage(event){
             console.log("new image: "+event.target.result);
+            var current = event.data.pageNum;
             var reader = new FileReader();
             reader.onload = function (event) {
                 var img = new Image();
                 img.src = event.target.result;
                 img.onload = function () {
-                    console.log("image load");
-                    $("#"+numUpload).attr("src",img.src);
+                    console.log("image load "+$("#"+current));
+                    if($("#"+current).attr("src") !=null){
+                        $("#"+current).attr("src",img.src);
+                    }
+                    else{
+                        $("#"+numUpload).attr("src",img.src);
+                        numUpload = numUpload+1;
+                    }
                 };
 
             };
             imgForm= new FormData();
             console.log("This is input "+imgForm);
             reader.readAsDataURL(event.target.files[0]);
-            alert("number of upload:" + numUpload);
-            numUpload = numUpload+1;
 
         }
 
         $("#pageImgPlus").click(function(){
+            var current = numUpload;
             var code =
                 "<div class='uploadEle col-sm-4'>" +
-                    "<img id="+numUpload+" name="+numUpload +"src='/img/chapter_default.jpeg'  onclick='$('.uploadPage').click();'>"+
-                    "<input class='uploadPage hide' id='input"+numUpload+"' class='hide' type='file' accept='image/*'>"+
-                    "<p class='pageNum'>Page"+numUpload+"</p>"+
+                    "<img id="+current+" name="+current +"src='/img/chapter_default.jpeg'  onclick='$('#'"+current+").click(newPage);'>"+
+                    "<input class='uploadPage hide' id='input"+current+"' class='hide' type='file' accept='image/*'>"+
+                    "<p class='pageNum'>Page"+current+"</p>"+
                 "</div>";
 
 
             $(".row-fluid").append(code);
-            $("#input"+numUpload).bind("change",newPage);
+            $("#input"+current).bind("change",{pageNum: current},newPage);
+            $("#"+current).bind("click", function(){
+                console.log("image is clicked and triggering input tag");
+                $("#input"+current).trigger("click");
+            });
 
-            $("#input"+numUpload).trigger("click");
+            $(".pageNum").bind("click", function(){
+                console.log("image is clicked and triggering input tag");
+                $("#input"+current).trigger("click");
+            });
 
-            console.log('been here right now'+numUpload);
-                //$("#p"+numUpload).val("Page "+numUpload);
-                //numUpload= numUpload+1;
-            console.log('after increase one in numUpload: '+numUpload);
-
+            $("#input"+current).trigger("click");
 
 
 
