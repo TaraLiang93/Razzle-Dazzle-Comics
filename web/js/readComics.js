9013e57a68cec045f0b7d1ce4d80a48217eba4eb
Jason <jasonfontaine18@gmail.com>
2016-05-14 13:15:43 -0400

    made changes to the comic series page also gotten started on the latestReleasePage

---
+++ web/js/readComics.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Jason on 5/14/16.
+ */
+
+$(document).ready(function() {
+
+    $(".series").each(function(){// this code needs to be changed
+        $(this).click(function(){
+            $.ajax({
+                url: "/read" + $(this).text(),
+                data : {seriesID : $(this).attr("id")},
+                async: false
+
+            })
+                .done(function(){
+                    console.log("wow all the data was sent over");
+                })
+                .fail(function(){
+                    console.log("The data failed to send");
+                });
+        });
+    })
+
+});
