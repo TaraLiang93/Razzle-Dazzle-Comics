7700403294e556da101c4b45773657d60ff92c91
TaraLiang93 <taraliang93@gmail.com>
2016-05-13 23:39:44 -0400

    comic series' jsp and js works. It should redirect to the apporiate read a chapter page.

--- web/js/comicSeries.js
+++ web/js/comicSeries.js
@@ -4,7 +4,8 @@
 $(document).ready(function(){
     $(".chapter").each(function(){
         $(this).click(function(){// this should redirect to 1st page of that chapter
-            $(location).attr("href","/read/"+$("#seriesTit").val()+"/"+ $(this).attr("id"));
+            alert("Clicker");
+            $(location).attr("href","/read/"+$("#seriesTit").text()+"/"+ $(this).attr("id"));
         });
     });
 });
