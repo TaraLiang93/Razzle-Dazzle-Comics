9d260411f5438866be1a0f4b741e9a7b3730e462
Jason <jasonfontaine18@gmail.com>
2016-05-14 14:39:58 -0400

    um idk for latestRelease

--- src/com/rdc/SearchController.java
+++ src/com/rdc/SearchController.java
@@ -1,6 +1,7 @@
 package com.rdc;
 
 import com.data.api.queries.external.GetSeriesOfUserDataCommand;
+import com.data.structure.Genre;
 import com.data.structure.Series;
 import com.google.appengine.api.users.UserServiceFactory;
 import org.springframework.stereotype.Controller;
@@ -26,7 +27,7 @@ public class SearchController {
 
 
         String searchBar = req.getParameter("searchBar");
-
+        Genre genre;
         map.put("searchTitle",searchBar);
 
         try {
