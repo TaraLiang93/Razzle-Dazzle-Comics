b5fdf1942b5d5b035dd15e4ca2e8a1818709176f
jamhuang <james.huang@stonybrook.edu>
2016-05-16 21:56:46 -0400

    fixed UpdateUserDataAddSeriesTask to take in the String ID

--- src/com/data/api/updatables/updateTasks/UpdateUserDataAddSeriesTask.java
+++ src/com/data/api/updatables/updateTasks/UpdateUserDataAddSeriesTask.java
@@ -5,7 +5,9 @@ import com.data.api.exceptions.CreateException;
 import com.data.api.exceptions.FetchException;
 import com.data.api.exceptions.UpdateException;
 import com.data.api.interfaces.Container;
+import com.data.api.interfaces.Readable;
 import com.data.api.interfaces.UpdateTask;
+import com.data.api.queries.external.GetSeriesByIDCommand;
 import com.data.structure.Series;
 
 import java.util.ArrayList;
@@ -16,17 +18,31 @@ import java.util.List;
  */
 public class UpdateUserDataAddSeriesTask implements UpdateTask<UserData> {
 
-    Series series;
+    Series series = null;
+    Long seriesID = null;
 
     public UpdateUserDataAddSeriesTask ( Series series){
         this.series = series;
-
     }
+
+    public UpdateUserDataAddSeriesTask (String seriesID){
+        try{
+            this.seriesID = Long.parseLong( seriesID);
+        }
+        catch ( NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+
+    public UpdateUserDataAddSeriesTask( Long seriesID){
+        this.seriesID = seriesID;
+    }
+
 
     @Override
     public List<UserData> update(Container<UserData> entity) throws UpdateException, FetchException, CreateException {
 
-        if(this.series == null || entity == null){
+        if( (this.series == null && seriesID == null) || entity == null){
             throw new UpdateException("UpdateUserDataAddSeriesTask seriesId or entity is null");
         }
 
@@ -34,8 +50,17 @@ public class UpdateUserDataAddSeriesTask implements UpdateTask<UserData> {
 
         if(userData == null)throw new UpdateException("User must have Data to continue!");
 
-        //updateUserData
-        userData.addSeriesToList(series.getKey());
+        if( series != null) {
+            //updateUserData
+            userData.addSeriesToList(series.getKey());
+
+        }
+        else{
+            Readable<Series> seriesReadable = new GetSeriesByIDCommand( seriesID );
+            Series series = seriesReadable.fetch().getResult();
+            userData.addSeriesToList(series.getKey());
+        }
+
 
         List<UserData> updateList = new ArrayList<>();
         updateList.add(userData);
